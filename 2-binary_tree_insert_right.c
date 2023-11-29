9df7a8632a2c197a3a1bc1d2217fee9abe13f38f
raxidbou4nja <rachid.boughnja@gmail.com>
2023-11-29 10:58:09 -0800

    fix errors

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,10 +16,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
+	new_node = binary_tree_node(parent, value);
+
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node = binary_tree_node(parent, value);
 
 	if (parent->right != NULL)
 	{
