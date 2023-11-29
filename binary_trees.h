f846aed4b6fe737f306721bb4bf44a4f3f5fe939
raxidbou4nja <rachid.boughnja@gmail.com>
2023-11-29 10:27:22 -0800

    update h

--- binary_trees.h
+++ binary_trees.h
@@ -19,7 +19,8 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-/* Function prototype for creating a binary tree node */
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
